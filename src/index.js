f0a1c326536a05a1c29ec3676778768dc20bd06b
murris <iemf.dev@gmail.com>
2025-04-06 23:48:48 -0400

    feature: add login

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const routes = require("./routes");
 const cors = require("cors");
 
 const app = express();
 
+app.use(express.json());
+
 app.use(cors());
 
 app.use(routes);
 
 app.listen(process.env.PORT, () => {
-  console.log("Server is running on http://localhost:3000");
+  console.log("Server is running on http://localhost:" + process.env.PORT);
 });
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,45 @@
 const { Router } = require("express");
+jwt = require("jsonwebtoken");
 
 const routes = Router();
+
+let users = [
+  {
+    name: "admin",
+    email: "admin@spsgroup.com.br",
+    type: "admin",
+    password: "1234"
+  }
+];
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const authenticate = (req, res, next) => {
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res.status(401).send("Token no proporcionado");
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send("Token inválido");
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+
+routes.post("/login", (req, res) => {
+  console.log("Request body:", req.body);
+  const { email, password } = req.body;
+  const user = users.find((user) => user.email === email && user.password === password);
+  if (!user) {
+    return res.status(401).send("Credenciais inválidas");
+  }
+  const token = jwt.sign({ id: user.email }, JWT_SECRET, { expiresIn: "1h" });
+  res.json({ token });
+});
+
 
 routes.get("/", (req, res) => {
   res.send("Hello World!");
